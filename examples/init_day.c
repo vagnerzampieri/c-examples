5b4f4324149b5672be2adaa862187b96a4403ab8
Vagner Zampieri <vgzampieri@gmail.com>
2022-12-18 20:33:28 -0300

    refact init_day

--- examples/init_day.c
+++ examples/init_day.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+  char * name;
+  char * value;
+} command;
+
 int main() {
-  printf("Start Spotify");
-  system("spotify &");
+  command commands[] = {
+    { "Spotify", "spotify &" },
+    { "Slack", "slack &" },
+    { "Brave", "brave-browser &" },
+    { "SaaS", "code ~/Projects/capim/capim-dash-backend &" },
+    { "BNPL", "code ~/Projects/capim/capim-backend &" },
+  };
 
-  printf("Start Slack");
-  system("slack &");
+  size_t size = sizeof(commands) / sizeof(command);
 
-  printf("Start Brave");
-  system("brave-browser &");
-
-  printf("Start SaaS");
-  system("code ~/Projects/capim/capim-dash-backend &");
-
-  printf("Start BNPL");
-  system("code ~/Projects/capim/capim-backend &");
+  for (size_t i = 0; i < size; i++) {
+    printf("Start %s", commands[i].name);
+    system(commands[i].value);
+  }
 
   return 0;
 }

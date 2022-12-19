b32aa0bad47361309682d67240718045875aeb3d
Vagner Zampieri <vgzampieri@gmail.com>
2022-12-19 10:18:52 -0300

    add cube

---
+++ examples/cube.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double cube(double num);
+
+int main() {
+  printf("Answer: %f\n", cube(3.0));
+
+  return 0;
+}
+
+double cube(double num) {
+  return num * num * num;
+}
